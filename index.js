e3e3f64c3c7c16bb725f43e97d0659a2397f6bfc
T. Vega <tsvega7@gmail.com>
2020-07-31 13:38:29 -0400

    Change Sounds & Letters Switch

--- index.js
+++ index.js
@@ -24,38 +24,38 @@ document.addEventListener("keydown", function (e) {
 function makeSound(key) {
    switch (key) {
 
-      case "w":
-         let snare = new Audio("sounds/snare.mp3");
-         snare.play();
-         break;
-
-      case "d":
-         let tom1 = new Audio("sounds/tom-1.mp3");
-         tom1.play();
-         break;
-
-      case "j":
-         let tom2 = new Audio("sounds/tom-2.mp3");
-         tom2.play();
-         break;
-
-      case "k":
-         let HitHat = new Audio("sounds/Hit-Hat.mp3");
-         HitHat.play();
-         break;
-
-      case "l":
-         let tom4 = new Audio("sounds/tom-4.mp3");
-         tom4.play();
-         break;
-
       case "a":
          let crash = new Audio("sounds/crash.mp3");
          crash.play();
          break;
 
       case "s":
-         let sticks = new Audio("sounds/Drum-Sticks-Hit-.mp3");
+         let HitHat = new Audio("sounds/Hit-Hat.mp3");
+         HitHat.play();
+         break;
+
+      case "d":
+         let tom2 = new Audio("sounds/tom-2.mp3");
+         tom2.play();
+         break;
+
+      case "w":
+         let snare = new Audio("sounds/snare.mp3");
+         snare.play();
+         break;
+
+      case "j":
+         let kick = new Audio("sounds/kick-bass.mp3");
+         kick.play();
+         break;
+
+      case "k":
+         let tom4 = new Audio("sounds/tom-4.mp3");
+         tom4.play();
+         break;
+
+      case "l":
+         let sticks = new Audio("sounds/Drum-Sticks.mp3");
          sticks.play();
          break;
 
